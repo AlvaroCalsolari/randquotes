6d6376a28e801a81d310302dd98653b1ae3c1847
AlvaroCalsolari <alvarocalsolari@hotmail.com>
2017-11-25 22:45:08 -0200

    Tweet button
    Botão que gera tweet adicionado

--- js/script.js
+++ js/script.js
@@ -3,9 +3,25 @@ function changeQuote(id){
 		$(id).html(data);
 	});
 }
+
+function getQuote(){
+	return $("#quote").text();
+}
+
+function tweetForMe(){
+	let url 		= "https://twitter.com/intent/tweet";
+	let text		= getQuote();
+	let hashtag = "randQuote";
+	let via 		= "randomQuotes";
+	window.open(url+"?text="+text+";hashtags="+hashtag+";via="+via,"" , "width=500,height=300");
+}
+
 $(function() {
 	changeQuote('#quote');
 	$("#new-quote").click(function(){
 		changeQuote('#quote');
+	});
+	$("#tweet").click(function(){
+		tweetForMe();
 	});	
 });
